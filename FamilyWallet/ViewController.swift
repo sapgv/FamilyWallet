b5433b1d988b5fab9f89ed50126b637221f5d47e
sapgv <grisha.sapgv@mail.ru>
2019-08-02 01:35:36 +0500

    Initial Commit

---
+++ FamilyWallet/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FamilyWallet
+//
+//  Created by Grigoriy Sapogov on 02/08/2019.
+//  Copyright © 2019 Grigoriy Sapogov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
